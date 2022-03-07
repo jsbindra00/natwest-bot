32f7a490a470fac2cbbb09a6c1289485c1470cc4
n1ghtrid3r <60359484+n1ghtrid3r@users.noreply.github.com>
2022-03-07 22:25:00 +0000

    Add files via upload

--- csvprocessor.py
+++ csvprocessor.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 from docutil import DocUtil
 from enum import IntEnum
+import re
+import datetime
 
 
 class ExpenditureColumn(IntEnum):
@@ -16,31 +18,73 @@ class ExpenditureColumn(IntEnum):
 arrivalsFolderLocation = r"D:\FILES\Desktop\other\Expenditure analysis\Analysis\2022\arrivals"
 
 
-def ProcessArrivalsFolder():
+def GetExpenditures(predicate, expendituresMatrix):
+    lst = []
+    for row in expendituresMatrix:
+        if predicate(row):
+            lst.append(row)
+    return np.array(lst)
+
+
+def UniformDateRegexReplacer(dateStr):
+    # replace all 2 digit dates with 4 digits
+    match = dateStr.group()
+    return match if len(match) == 4 else "20" + match
+
+
+
+dateMapping = {"jan" : "01", "feb" : "02", "mar" : "03", "apr" : "04", "may" : "05", "jun" : "06", "jul" : "07", "aug" : "08", "sep" : "09", "oct" : "10", "nov" : "11", "dec" : "12"}
+
+def CleanDate(dateStr):
+    dateStr = re.sub("-", "/", dateStr.lower())
+    # 01/jan/2022 -> 01/01/2022
+    dateStr = re.sub('[a-z]{3}', lambda x: dateMapping[x.group()], dateStr)
+    # 01/01/22 -> 01/01/2022
+    dateStr = re.sub("(?![0-9]*\/).+", UniformDateRegexReplacer, dateStr)
+    return dateStr
+
+
+def StackCSVs():
     csvFiles = [file for file in DocUtil.ListAllFiles(arrivalsFolderLocation) if DocUtil.GetExtension(file) == ".csv"]
     tempMaster = None
     for file in csvFiles:
-        print(file)
         expenditures = pd.read_csv(file, sep=',', header=None,keep_default_na=False, index_col=False).to_numpy()
         tempMaster = np.vstack([tempMaster, expenditures]) if tempMaster is not None else expenditures
-
-    print(tempMaster)
-
-
+    return tempMaster
+def CleanMatrix(tempMaster):
     i = 0
     while i < tempMaster.shape[0]:
         row = tempMaster[i]
         try:
             float(str(row[ExpenditureColumn.Value]))
+            row[ExpenditureColumn.Date] = CleanDate(row[ExpenditureColumn.Date])
             i += 1
         except(Exception):
-            print(row)
             tempMaster = np.delete(tempMaster, i, 0)
             i = i - 1 if i > 0 else 0
-    pd.DataFrame(tempMaster).to_csv("foo.csv", index=False, header=False)
+    
+            
+    return tempMaster
+def SortMatrix(tempMaster):
+    return np.array(sorted(list(tempMaster), key=lambda row: datetime.datetime.strptime(row[0], '%d/%m/%Y')))
+
+
+def ProcessArrivalsFolder():
+
+    tempMaster = StackCSVs()
+    tempMaster = CleanMatrix(tempMaster)
+    tempMaster = SortMatrix(tempMaster)
+    
+    pd.DataFrame(tempMaster).to_csv("processedarrivals.csv", index=False, header=False)
+
+
+    return tempMaster
 
     
 
 
 
-ProcessArrivalsFolder()
+def GetAllFoodCosts(row):
+    return 1
+
+monthlyExpenditures = ProcessArrivalsFolder()
